efb7253531f0ec1a4fae313d3fa2cd390703006d
Matteo Magliano <matteo.magliano01@gmail.com>
2019-12-18 15:21:22 +0100

    inserimento km

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
 alert('hello')
+// Il programma dovrà chiedere all’utente il
+// numero di chilometri che vuole
+// percorrere e l’età del passeggero.
+// Sulla base di queste informazioni dovrà
+// calcolare il prezzo totale del viaggio.
+// Il prezzo del biglietto è definito in base ai
+// km (0.21 € al km), ma va applicato uno
+// sconto del 20% per i minorenni e del
+// 40% per gli over 65.
+
+
+//inserire chilometri da percorrere
+var inserireKm = parseInt(prompt('Quanti km vuoi percorrere?'));
+console.log(inserireKm);
